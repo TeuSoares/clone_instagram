1d38c4c819f76a78f6339baf0366bed88cd66b08
Mateus Soares <70549313+TeuSoares@users.noreply.github.com>
2022-01-18 15:59:50 -0300

    Update funcoes.php

--- PHP/funcoes.php
+++ PHP/funcoes.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor = "robb0466.publiccloud.com.br:3306";      // Nome ou IP do servido. Onde está?
-$usuario = "limei_mateus";
-$senha = "@a1b2c3d4#";
-$banco = "limeiraweb_mateus";
+$servidor = "localhost";      // Nome ou IP do servido. Onde está?
+$usuario = "root";
+$senha = "";
+$banco = "instagram";
 $conexao = new mysqli($servidor,$usuario,$senha,$banco);   // Criando conexão com o banco
 mysqli_query($conexao,"SET NAMES 'utf8'");  
 mysqli_query($conexao,'SET character_set_connection=utf8');  
